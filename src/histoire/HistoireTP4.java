9b8ba58c798c619f4664765f978b021c32b274a6
Alicia Usabiaga <sbl4254a@ensfsi.onmicrosoft.com>
2025-03-27 11:26:58 +0100

    TP4 yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,12 +2,14 @@ package histoire;
 
 import personnages.Humain;
 import personnages.Commercant;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
 	
 	public static void main(String[] args) {
 		Humain prof = new Humain("Prof", "kombucha", 54);
 		Commercant marchand = new Commercant("Marco", "thé", 20);
+		Yakuza yaku = new Yakuza("Yaku", "biere", 30);
 		
 		prof.direBonjour();
 		prof.acheter("boisson", 12);
@@ -16,10 +18,15 @@ public class HistoireTP4 {
 		prof.acheter("kimono", 50);
 		
 		System.out.println();
-		
 		marchand.direBonjour();
 		marchand.seFaireExtorquer();
 		marchand.recevoir(15);
 		marchand.boire();
+		
+		System.out.println();
+		yaku.direBonjour();
+		yaku.extorquer(marchand);
+				
+		
 	}
 }
